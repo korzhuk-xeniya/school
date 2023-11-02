00deb872f5e001bb9a0f8ecf45fd957fa19ac719
Ксения <korzhuk.xeniya@yandex.ru>
2023-11-02 23:44:14 +0300

    переопределены методы equals и hashCode

---
+++ src/test/java/ru/hogwarts/school/service/StudentServiceImplTest.java
@@ -0,0 +1,29 @@
+package ru.hogwarts.school.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceImplTest {
+    private  StudentServiceImpl underTest = new  StudentServiceImpl();
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void ageSorter() {
+    }
+}
