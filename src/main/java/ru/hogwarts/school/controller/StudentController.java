ce6fe97a19685cc0f61dbc1ba2af9fe1b3701cb1
Ксения <korzhuk.xeniya@yandex.ru>
2023-12-15 22:24:35 +0300

    4.6 done

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -72,4 +72,16 @@ public class StudentController {
     public  Double getAllStudentsAvgAge () {
         return studentService.getAllStudentAvgAge();
     }
+    @GetMapping("parallel-thread")
+    public void getNames() throws InterruptedException {
+        studentService.getStudentNames();
+    }
+    @GetMapping("sync-thread")
+    public void getNamesSync() throws InterruptedException {
+        studentService.getStudentNamesSync();
+    }
+    @GetMapping("sync-thread2")
+    public void getNamesSyncWait() throws InterruptedException {
+        studentService.getStudentNamesWait();
+    }
 }
