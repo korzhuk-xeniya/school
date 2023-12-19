807cb001c015bdeff16b46a73e3984d671a98155
Ксения <korzhuk.xeniya@yandex.ru>
2023-12-19 21:03:29 +0300

    4.5 done

--- src/main/resources/liquibase/scripts/4.3-migration-and-index.sql
+++ src/main/resources/liquibase/scripts/4.3-migration-and-index.sql
@@ -1,3 +1,3 @@
 CREATE INDEX IF NOT EXISTS student_name_index ON student (name);
-CREATE INDEX IF NOT EXISTS faculty_name_color ON student (name, color);
+CREATE INDEX IF NOT EXISTS faculty_name_color ON faculty (name, color);
 
